fc1cfdf30db88fdf31a8f4c3777d08dc873c49fe
C-Kitching <113296322+C-Kitching@users.noreply.github.com>
2022-12-03 16:05:07 +0000

    day 3

--- Day3/day3_part2.cpp
+++ Day3/day3_part2.cpp
@@ -18,27 +18,30 @@ int main()
     std::string file_path{"../Day3/day3_data.txt"};
     std::ifstream my_file(file_path);
     std::string line;
-    std::vector<std::string> backpack;
+    std::vector<std::vector<std::string>> groups;
     if (my_file.is_open()) {
+        std::vector<std::string> group;
+        int counter{0};
         while (std::getline(my_file, line)) {
-            backpack.push_back(line);
+            group.push_back(line);
+            counter++;
+            if(counter == 3){
+                groups.push_back(group);
+                group.clear();
+                counter = 0;
+            }
         }
     }
 
-    // extract all items
-    std::string first_part, second_part;
-    int N;
+    // loop through groups
     int score{0};
-    for(std::string& item : backpack){
+    for(auto& group : groups){
 
-        // split string
-        N = item.size();
-        first_part = item.substr(0, N/2);
-        second_part = item.substr(N/2, N);
+        // check each char of first string
+        for(char& c : group[0]){
 
-        // loop through first string and attempt to find occurences in second
-        for(char& c : first_part){
-            if(second_part.find(c) != std::string::npos){
+            // if char in other two strings
+            if(group[1].find(c) != std::string::npos && group[2].find(c) != std::string::npos){
                 if(std::islower(c)) score += c - 'a' + 1;
                 else score += std::tolower(c) - 'a' + 27;
                 break;
