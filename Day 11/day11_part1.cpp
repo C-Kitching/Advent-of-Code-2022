e9bfee7080e46ca8df6bc0e22ba1e63747fb8748
Christopher Kitching <113296322+MacSwish@users.noreply.github.com>
2022-12-12 12:55:11 +0000

    Day 11

--- Day 11/day11_part1.cpp
+++ Day 11/day11_part1.cpp
@@ -16,36 +16,120 @@
 
 using namespace std;
 
-int main()
-{
-    // decalre variables
-    string buffer;
+// enum class for operations of monkeys
+enum operation_enum{e_add, e_mul, e_squ};
+
+// monkey structure
+struct monkey{
+    vector<int> items;  // items held by monkey
+    operation_enum op;  // operation of monkey
+    int op_arg;  // number involved in operation
+    int test_arg;  // number to divide by in test
+    int true_num;  // monkey to throw to if passed test
+    int false_num;  // monkey to throw to if failed test
+    int inspection_count;  // number of inspections made by monkey
+};
+
+// short hand for vector of monkeys
+using monkeys = vector<monkey>;
+
+// function to split string into tokens
+vector<string> split(const string& s){
+    string token;
+    vector<string> tokens;
+    stringstream ss(s);
+    while(ss >> token) tokens.push_back(token);
+    return tokens;
+}
+
+// function to parse data
+monkeys read_input(const string& file){
+
+    monkeys ret;
+    ifstream fs(file);
     string line;
-    string temp_s;
-    int temp_n;
-    char temp_c;
-    vector<vector<int>> monkeys_items;
-    vector<int> monkeys_operations;
-    vector<int> monkeys_test;
-    vector<pair<int, int>> monkeys_to_throw_to;
 
-    // read in data
-    string file_path{"../Day10/day10_data.txt"};
-    ifstream my_file(file_path);
-    if(my_file.is_open()) {
-        while(getline(my_file, line)) {
-            stringstream ss(line);
+    // read each line of file
+    while(getline(fs, line)){
 
-            ss >> temp_s;
-            if(temp_s == "Monkey"){
-                ss >> temp_n;
-                current_monkey = temp_n;
+        // split line into tokens
+        auto tokens = split(line);
+
+        if(!tokens.empty()){
+
+            // new monkey
+            if(tokens[0] == "Monkey"){
+                ret.push_back(monkey());
             }
 
+            // items held by monkey
+            else if(tokens[0] == "Starting"){
+                for(int i{2}; i < tokens.size(); i++){
+                    ret.back().items.push_back(stoi(tokens[i]));
+                }
+            }
 
+            // operations performed by monkey
+            else if(tokens[0] == "Operation:"){
 
+                // squaring
+                if(tokens[5] == "old"){
+                    ret.back().op = e_squ;
+                    ret.back().op_arg = 0;
+                }
 
-        }  
+                // multiplication
+                else if(tokens[4] == "*"){
+                    ret.back().op = e_mul;
+                    ret.back().op_arg = stoi(tokens[5]);
+                }
+
+                // addition
+                else if(tokens[4] == "+"){
+                    ret.back().op = e_add;
+                    ret.back().op_arg = stoi(tokens[5]);
+                }
+
+            }
+
+            // test condition
+            else if(tokens[0] == "Test"){
+                ret.back().test_arg = stoi(tokens[3]);
+            } 
+
+            // true result
+            else if(tokens[1] == "true:"){
+                ret.back().true_num = stoi(tokens[5]);
+            }
+
+            // false result
+            else if(tokens[1] == "false:"){
+                ret.back().false_num = stoi(tokens[5]);
+            }
+        }
     }
+
+    return ret;
+}
+
+// function to perform calculation
+auto process(monkeys monkeys, const bool& part1, const int& rounds){
+
+    int mod_product{1};
+    for(auto& monkey : monkeys){
+        mode_product *= monkey.test_arg;
+    }
+
+    for(int r{0}; r < rounds; r++){
+        
+    }
+
+
+}
+
+int main()
+{
+
+
     return 0;
 }
